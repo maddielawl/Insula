d69ff29edfe954000a3cbbc55fe366d68dc451ad
Emilien Bertrand <58692462+EtCtulu@users.noreply.github.com>
2021-01-13 18:47:56 +0100

    Avancements sur le 2D Controller

---
+++ Insigna_Game/Assets/Scripts/Player/PlayerStates/SuperStates/PlayerGroundedState.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGroundedState : PlayerState
+{
+    
+    protected Vector2 PlayerGroundedState
+    
+    public PlayerGroundedState(Player player, PlayerStateMachine stateMachine, PlayerData playerData, string animBoolName) : base(player, stateMachine, playerData, animBoolName)
+    {
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+
+        input = player.InputHandler.MovementInput;
+    }
+
+    public override void PhysicsUpdate()
+    {
+        base.PhysicsUpdate();
+    }
+
+    public override void DoChecks()
+    {
+        base.DoChecks();
+    }
+}
