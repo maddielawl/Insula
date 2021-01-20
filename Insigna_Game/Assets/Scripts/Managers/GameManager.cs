7827d46568aaec7f6b90530e88bc5a630c085ae9
Emilien Bertrand <58692462+EtCtulu@users.noreply.github.com>
2021-01-20 09:09:54 +0100

    GameManager crée

---
+++ Insigna_Game/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    
+    #region Singlton:Profile
+
+    public static GameManager Instance;
+
+    void Awake ()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy (gameObject);
+    }
+    #endregion
+
+    [Header("Player Stats")]
+    public int playerHealth;
+    public int playerSanity;
+    public int playerMadness;
+    
+}
