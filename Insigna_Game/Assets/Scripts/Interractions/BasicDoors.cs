c7657e3315733951c0cee47688c40404fbfbe38c
Madeline <bertrandemilien@gmail.com>
2021-03-15 10:19:48 +0100

    Oui

---
+++ Insigna_Game/Assets/Scripts/Interractions/BasicDoors.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicDoors : MonoBehaviour
+{
+    private Interractable parent;
+    public Transform tpPoint;
+
+    private GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        parent = transform.parent.GetComponent<Interractable>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (parent.interractionSecurity == false)
+        {
+            parent.interractionSecurity = true;
+            player.transform.position = tpPoint.position;
+
+        }
+
+    }
+}
