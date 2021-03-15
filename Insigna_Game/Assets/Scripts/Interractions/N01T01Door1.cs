73fe9083e1af67642f0fa01870380148128d3779
Madeline <bertrandemilien@gmail.com>
2021-03-15 21:12:00 +0100

    ui

---
+++ Insigna_Game/Assets/Scripts/Interractions/N01T01Door1.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class N01T01Door1 : MonoBehaviour
+{
+    private Interractable parent;
+    public Transform tpPoint;
+    public bool isLeverOn = false;
+
+    private GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        parent = transform.parent.GetComponent<Interractable>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (parent.interractionSecurity == false)
+        {
+            parent.interractionSecurity = true;
+            if (isLeverOn == true)
+            {
+                player.transform.position = tpPoint.position;
+            }
+
+        }
+
+    }
+}
