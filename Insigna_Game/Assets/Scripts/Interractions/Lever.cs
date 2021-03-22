e5fcec1f614c9229a333858b440ef91445985ac7
Madeline <bertrandemilien@gmail.com>
2021-03-22 11:28:48 +0100

    Bugfix, utilisation unique de l'inventaire

--- Insigna_Game/Assets/Scripts/Interractions/Lever.cs
+++ Insigna_Game/Assets/Scripts/Interractions/Lever.cs
@@ -29,6 +29,7 @@ public class Lever : MonoBehaviour
                     UIManager.Instance.inventoryButton1.sprite = baseSlotSprite.sprite;
                     UIManager.Instance.objectInSlot1 = emptySlot;
                     UIManager.Instance.isSlot1Active = false;
+                    UIManager.Instance.oneSlotAtTheTimeSecurity = false;
                     UIManager.Instance.object1Equipped.SetActive(false);
                     FindObjectOfType<AudioManager>().Play("UseLever");
                     door.GetComponent<N01T01Door>().isLeverOn = true;
@@ -43,6 +44,8 @@ public class Lever : MonoBehaviour
                     UIManager.Instance.inventoryButton2.sprite = baseSlotSprite.sprite;
                     UIManager.Instance.objectInSlot2 = emptySlot;
                     UIManager.Instance.isSlot2Active = false;
+                    UIManager.Instance.oneSlotAtTheTimeSecurity = false;
+
                     UIManager.Instance.object2Equipped.SetActive(false);
                     FindObjectOfType<AudioManager>().Play("UseLever");
                     door.GetComponent<N01T01Door>().isLeverOn = true;
@@ -56,6 +59,7 @@ public class Lever : MonoBehaviour
                     UIManager.Instance.inventoryButton3.sprite = baseSlotSprite.sprite;
                     UIManager.Instance.objectInSlot3 = emptySlot;
                     UIManager.Instance.isSlot3Active = false;
+                    UIManager.Instance.oneSlotAtTheTimeSecurity = false;
                     UIManager.Instance.object3Equipped.SetActive(false);
                     FindObjectOfType<AudioManager>().Play("UseLever");
                     door.GetComponent<N01T01Door>().isLeverOn = true;
