99d4e5172fc96f9f7c76e995a5e0b9f593c3d6fc
Vincent <vincentpy0902@gmail.com>
2021-03-24 16:41:22 +0100

    J'ai remis une seule cam qui affiche l'ui + particule sur le main menu mais le curseur est tjrs en dessous de l'ui je deviens fou

--- Insigna_Game/Assets/Scripts/Managers/MenusManager.cs
+++ Insigna_Game/Assets/Scripts/Managers/MenusManager.cs
@@ -11,6 +11,8 @@ public class MenusManager : MonoBehaviour
 {
     public GameObject cursorManger;
 
+    public GameObject MainMenuCamera;
+
     private GameInputs menusActions;
     private AsyncOperation asyncOp;
     
@@ -295,7 +297,8 @@ public class MenusManager : MonoBehaviour
         asyncOp.allowSceneActivation = true;
         loadingScreen.SetActive(false);
         ingameMainUI.SetActive(true);
-        // transform.GetChild(0).GetComponent<Canvas>().renderMode = RenderMode.ScreenSpaceOverlay;
+        transform.GetChild(0).GetComponent<Canvas>().renderMode = RenderMode.ScreenSpaceOverlay;
+        MainMenuCamera.SetActive(false);
         menusActions.MainMenuActions.ValidateLoadScene.started -= HideLoadingScreen;
         DeactivateMainMenuActions();
         GameManager.Instance.ActivateInGameActions();
