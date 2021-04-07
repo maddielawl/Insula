c365ee0ace8b9ba740d7ff0926cd43a3896037de
Madeline <bertrandemilien@gmail.com>
2021-04-07 16:32:02 +0200

    0635143186 appelez moi

--- Insigna_Game/Assets/Scripts/Interractions/ButtonL02.cs
+++ Insigna_Game/Assets/Scripts/Interractions/ButtonL02.cs
@@ -4,8 +4,26 @@ using UnityEngine;
 
 public class ButtonL02 : MonoBehaviour
 {
+    private Interractable parent;
+
     public int lever1 = 0;
     public int lever2 = 0;
     public int lever3 = 0;
     public int lever4 = 0;
-}
+
+    private void Start()
+    {
+        parent = transform.parent.GetComponent<Interractable>();
+
+    }
+
+    void Update()
+    {
+        if (parent.interractionSecurity == false)
+        {
+            if (lever1 == 2 && lever2 == 1 && lever3 == 2 && lever4 == 0)
+            {
+                Debug.Log("Ta grosse mère vincent");
+            }
+        }
+    }
