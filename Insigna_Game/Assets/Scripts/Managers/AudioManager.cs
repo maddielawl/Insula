1d6861f220a742186bd2e1a2ceab67f0c5811826
Antonin <antoninfouquet.pro@gmail.com>
2021-04-17 16:47:19 +0200

    Auto stash before revert of "Fmod project inital setup"

--- Insigna_Game/Assets/Scripts/Managers/AudioManager.cs
+++ Insigna_Game/Assets/Scripts/Managers/AudioManager.cs
@@ -31,10 +31,4 @@ public class AudioManager : MonoBehaviour
         Sound s = Array.Find(sounds, sound => sound.name == name);
         s.source.Stop();
     }
-
-    
-    public void PlayOneShot(string soundPath)
-    {
-        FMODUnity.RuntimeManager.PlayOneShot(soundPath);
-    }
 }
