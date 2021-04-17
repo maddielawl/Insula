3a90b9b9b09b0300832d07360668399cfc66ae26
Digzeblue <46719379+Digzeblue@users.noreply.github.com>
2021-04-17 16:28:20 +0200

    Audio manager cleaner code

--- Insigna_Game/Assets/Scripts/Managers/AudioManager.cs
+++ Insigna_Game/Assets/Scripts/Managers/AudioManager.cs
@@ -31,4 +31,10 @@ public class AudioManager : MonoBehaviour
         Sound s = Array.Find(sounds, sound => sound.name == name);
         s.source.Stop();
     }
+
+    
+    public void PlayOneShot(string soundPath)
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(soundPath);
+    }
 }
