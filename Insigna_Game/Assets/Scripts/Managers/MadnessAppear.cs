7b420cce3c1a6079d69db541d8d7c535c0688d7b
Madeline <bertrandemilien@gmail.com>
2021-04-26 15:02:22 +0200

    Ajout de la transition entre la folie et le monde normal avec un magnifique script auto

---
+++ Insigna_Game/Assets/Scripts/Managers/MadnessAppear.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MadnessAppear : MonoBehaviour
+{
+    [HideInInspector]
+    public List<GameObject> arrayobjects = new List<GameObject>();
+    [HideInInspector]
+    public List<SpriteRenderer> arraysprites = new List<SpriteRenderer>();
+    [HideInInspector]
+    public Transform arrayparent;
+
+
+    private void Start()
+    {
+        arrayparent = GameObject.FindGameObjectWithTag("Indicible").GetComponent<Transform>();
+        GameManager.Instance.indicible = this.gameObject.GetComponent<MadnessAppear>();
+
+        for (int i = 0; i < arrayparent.childCount; i++)
+        {
+            arrayobjects.Add(arrayparent.GetChild(i).gameObject);
+            arraysprites.Add(arrayparent.GetChild(i).GetComponent<SpriteRenderer>());
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            SetAppear();
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            SetDisAppear();
+        }
+    }
+
+    public void SetAppear()
+    {
+        
+        for (int i = 0; i < arrayparent.childCount; i++)
+        {
+            LeanTween.value(arrayobjects[i], SetSpriteAlpha, 0f, 1f,1f);
+        }
+        
+    }
+    public void SetDisAppear()
+    {
+
+        for (int i = 0; i < arrayparent.childCount; i++)
+        {
+            LeanTween.value(arrayobjects[i], SetSpriteAlpha, 1f, 0f, 1f);
+        }
+
+    }
+
+    public void SetSpriteAlpha(float val)
+    {
+        for (int i = 0; i < arrayparent.childCount; i++)
+        {
+            arraysprites[i].color = new Color(1f, 1f, 1f, val);
+        }
+    }
+
+}
