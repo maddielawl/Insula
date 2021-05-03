c41ed80bf05c4009866e779bbcc028d1cf142046
Vincent <vincentpy0902@gmail.com>
2021-05-03 14:23:21 +0200

    j'essaye de faire marcher l'indicible dans le level 2

--- Insigna_Game/Assets/Scripts/Managers/GameManager.cs
+++ Insigna_Game/Assets/Scripts/Managers/GameManager.cs
@@ -25,6 +25,9 @@ public class GameManager : MonoBehaviour
         else
             Destroy(gameObject);
         playerInput = new PlayerInputs();
+
+        madnessZone = GameObject.FindGameObjectWithTag("MadnessZone");
+        sanityZone = GameObject.FindGameObjectWithTag("SanityZone");
     }
     #endregion
 
@@ -151,8 +154,11 @@ public class GameManager : MonoBehaviour
                     {
                         once = true;
                     }
-                    madnessZone.SetActive(false);
-                    sanityZone.SetActive(true);
+                    if (madnessZone != null && sanityZone != null)
+                    {
+                        madnessZone.SetActive(false);
+                        sanityZone.SetActive(true);
+                    }
                     globalInterractionSecurity = false;
                     dimensionSwapNormal = false;
                     dimensionSwapMadness = true;
