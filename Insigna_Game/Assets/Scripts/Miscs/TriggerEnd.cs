11fe968fdc4b4a8253675a12d12e3f38f5031c56
Corbeau <sassycorbeau@gmail.com>
2021-05-10 09:52:31 +0200

    Début blocking level 4

---
+++ Insigna_Game/Assets/Scripts/Miscs/TriggerEnd.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerEnd : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("insert something cool here");
+    }
+}
