39ed28033d4fd32544fb3dac325f46e44350d0e3
Madeline <bertrandemilien@gmail.com>
2021-05-17 14:53:16 +0200

    Interractions rajoutées

---
+++ Insigna_Game/Assets/Scripts/Interractions/N03T03/PerchePerche.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PerchePerche : MonoBehaviour
+{
+    public InterractableWithInventory parent;
+    public Image baseSlotSprite;
+    public GameObject emptySlot;
+
+    public GameObject pouliedubas;
+
+    void Start()
+    {
+        parent = transform.parent.GetComponent<InterractableWithInventory>();
+    }
+
+    void Update()
+    {
+
+        parent.interractionSecurity = true;
+        if (UIManager.Instance.isSlot1Active == true)
+        {
+            if (UIManager.Instance.objectInSlot1.name.Contains(parent.objectToInterractWith))
+            {
+                UIManager.Instance.isSlot1Active = false;
+                UIManager.Instance.oneSlotAtTheTimeSecurity = false;
+                UIManager.Instance.object1Equipped.SetActive(false);
+                pouliedubas.SetActive(true);
+            }
+        }
+
+        if (UIManager.Instance.isSlot2Active == true)
+        {
+            if (UIManager.Instance.objectInSlot2.name.Contains(parent.objectToInterractWith))
+            {
+                UIManager.Instance.isSlot2Active = false;
+                UIManager.Instance.oneSlotAtTheTimeSecurity = false;
+                UIManager.Instance.object2Equipped.SetActive(false);
+                pouliedubas.SetActive(true);
+            }
+        }
+
+        if (UIManager.Instance.isSlot3Active == true)
+        {
+            if (UIManager.Instance.objectInSlot3.name.Contains(parent.objectToInterractWith))
+            {
+                UIManager.Instance.isSlot3Active = false;
+                UIManager.Instance.oneSlotAtTheTimeSecurity = false;
+                UIManager.Instance.object3Equipped.SetActive(false);
+                pouliedubas.SetActive(true);
+            }
+        }
+    }
+}
+    
