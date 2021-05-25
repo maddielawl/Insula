c8b05187ddf34251c384be55473d999b567f4f45
Vincent <vincentpy0902@gmail.com>
2021-05-25 13:23:47 +0200

    Désactivation des interactions quand on est dans les vents

--- Insigna_Game/Assets/Scripts/Interractions/N02T01/Vents.cs
+++ Insigna_Game/Assets/Scripts/Interractions/N02T01/Vents.cs
@@ -13,6 +13,11 @@ public class Vents : MonoBehaviour
     public RuntimeAnimatorController characterBasicAC;
     public AnimatorOverrideController characterVentAOC;
 
+    private GameObject[] allBoxColliders;
+
+    private BoxCollider2D[] allBoxCollier2DDisabled = new BoxCollider2D[100];
+    private int length = 0;
+
     private void Start()
     {
         playerAnim = GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>();
@@ -25,6 +30,32 @@ public class Vents : MonoBehaviour
         outside.SetActive(false);
         inside.SetActive(true);
         playerAnim.runtimeAnimatorController = characterVentAOC;
+
+        allBoxColliders = GameObject.FindGameObjectsWithTag("Interractable");
+        for (int i = 0; i < allBoxColliders.Length; i++)
+        {
+            if (allBoxColliders[i].GetComponent<BoxCollider2D>() != null)
+            {
+                if (allBoxColliders[i].GetComponent<BoxCollider2D>().enabled == false)
+                {
+                    Debug.Log(allBoxColliders[i].name);
+                    allBoxCollier2DDisabled[length] = allBoxColliders[i].GetComponent<BoxCollider2D>();
+                    length += 1;
+                }
+
+                if (allBoxColliders[i].GetComponent<BoxCollider2D>().enabled)
+                {
+                    allBoxColliders[i].GetComponent<BoxCollider2D>().enabled = false;
+                }
+
+                if(allBoxColliders[i].name == "HealthPack 2")
+                {
+                    allBoxColliders[i].GetComponent<BoxCollider2D>().enabled = true;
+                }
+            }
+        }
+        
+
     }
 
     public void GetOutside(GameObject thenear)
@@ -33,6 +64,17 @@ public class Vents : MonoBehaviour
         inside.SetActive(false);
         outside.SetActive(true);
         playerAnim.runtimeAnimatorController = characterBasicAC;
+
+        for (int i = 0; i < allBoxColliders.Length; i++)
+        {
+            allBoxColliders[i].GetComponent<BoxCollider2D>().enabled = true;
+        }
+
+        for (int i = 0; i < allBoxCollier2DDisabled.Length; i++)
+        {
+            allBoxCollier2DDisabled[i].GetComponent<BoxCollider2D>().enabled = false;
+        }
+        length = 0;
     }
 
 }
