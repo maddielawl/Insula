7837b51ba8929e5c6a91780c6a991e698656b2ab
Vincent <vincentpy0902@gmail.com>
2021-06-22 11:25:53 +0200

    suite du choix de fin

--- Insigna_Game/Assets/Scripts/Miscs/TriggerEnd.cs
+++ Insigna_Game/Assets/Scripts/Miscs/TriggerEnd.cs
@@ -32,7 +32,7 @@ public class TriggerEnd : MonoBehaviour
 
             CameraManager.Instance.setCameraPrioHigh(newCam);
             CameraManager.Instance.setCameraPrioLow(oldCam);
-            
+
             endCinematic.GetComponent<Animator>().SetTrigger("End");
 
             StartCoroutine(AfterEndAnim());
@@ -42,44 +42,93 @@ public class TriggerEnd : MonoBehaviour
     IEnumerator AfterEndAnim()
     {
         yield return new WaitForSeconds(14f);
+
+        UIManager.Instance.DisplayPortrait(0);
         cinematicTexts[0].SetActive(true);
-        yield return new WaitForSeconds(3f);
+
+        yield return new WaitForSeconds(6f);
+
         cinematicTexts[0].SetActive(false);
         cinematicTexts[1].SetActive(true);
-        yield return new WaitForSeconds(3f);
+
+        yield return new WaitForSeconds(6f);
+
+        UIManager.Instance.HidePortraits();
+        UIManager.Instance.DisplayPortrait(2);
         cinematicTexts[1].SetActive(false);
         cinematicTexts[2].SetActive(true);
-        yield return new WaitForSeconds(3f);
+
+        yield return new WaitForSeconds(6f);
+
         cinematicTexts[2].SetActive(false);
         cinematicTexts[3].SetActive(true);
-        yield return new WaitForSeconds(3f);
+
+        yield return new WaitForSeconds(6f);
+
+        UIManager.Instance.HidePortraits();
+        UIManager.Instance.DisplayPortrait(3);
         cinematicTexts[3].SetActive(false);
         cinematicTexts[4].SetActive(true);
-        yield return new WaitForSeconds(3f);
+
+        yield return new WaitForSeconds(6f);
+
+        UIManager.Instance.HidePortraits();
+        UIManager.Instance.DisplayPortrait(0);
         cinematicTexts[4].SetActive(false);
         cinematicTexts[5].SetActive(true);
-        yield return new WaitForSeconds(3f);
+
+        yield return new WaitForSeconds(6f);
+
+        UIManager.Instance.HidePortraits();
+        UIManager.Instance.DisplayPortrait(2);
         cinematicTexts[5].SetActive(false);
         cinematicTexts[6].SetActive(true);
-        yield return new WaitForSeconds(3f);
+
+        yield return new WaitForSeconds(6f);
+
+        UIManager.Instance.HidePortraits();
+        UIManager.Instance.DisplayPortrait(0);
         cinematicTexts[6].SetActive(false);
         cinematicTexts[7].SetActive(true);
-        yield return new WaitForSeconds(3f);
+
+        yield return new WaitForSeconds(6f);
+
+        UIManager.Instance.HidePortraits();
+        UIManager.Instance.DisplayPortrait(2);
         cinematicTexts[7].SetActive(false);
         cinematicTexts[8].SetActive(true);
-        yield return new WaitForSeconds(3f);
+
+        yield return new WaitForSeconds(6f);
+
+        UIManager.Instance.HidePortraits();
+        UIManager.Instance.DisplayPortrait(0);
         cinematicTexts[8].SetActive(false);
         cinematicTexts[9].SetActive(true);
-        yield return new WaitForSeconds(3f);
+
+        yield return new WaitForSeconds(6f);
+
+        UIManager.Instance.HidePortraits();
+        UIManager.Instance.DisplayPortrait(2);
         cinematicTexts[9].SetActive(false);
         cinematicTexts[10].SetActive(true);
-        yield return new WaitForSeconds(3f);
+
+        yield return new WaitForSeconds(6f);
+
+        UIManager.Instance.HidePortraits();
+        UIManager.Instance.DisplayPortrait(0);
         cinematicTexts[10].SetActive(false);
         cinematicTexts[11].SetActive(true);
-        yield return new WaitForSeconds(3f);
+
+        yield return new WaitForSeconds(6f);
+
+        UIManager.Instance.HidePortraits();
+        UIManager.Instance.DisplayPortrait(2);
         cinematicTexts[11].SetActive(false);
         cinematicTexts[12].SetActive(true);
-        yield return new WaitForSeconds(3f);
+
+        yield return new WaitForSeconds(6f);
+
+        UIManager.Instance.HidePortraits();
         choix.SetActive(true);
     }
 }
